46fdf567b93d0f31e8667b345542405e5b250f2c
stevenwoah <stevenwoah@users.noreply.github.com>
2019-08-05 23:02:31 -0700

    See if SSH_CONNECTION is better than SSH_CLIENT

--- bin/status-right.sh
+++ bin/status-right.sh
@@ -2,7 +2,7 @@
 
 STATUS_LINE='#[fg=#4800b5,bg=#4800b5,nobold,nounderscore,noitalics]#[fg=colour121,bg=#4800b5] %a  %b %d  %r #[fg=#6e61ff,bg=#4800b5,nobold,nounderscore,noitalics]#[fg=#ffffff,bg=#6e61ff] '
 
-if (( ${+SSH_CLIENT} )); then
+if (( ${+SSH_CONNECTION} )); then
   STATUS_LINE+="#H"
 else
   STATUS_LINE+="#h"
