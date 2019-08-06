bc76443eb9bb43a191d0f021e98a070aed5fb524
stevenwoah <stevenwoah@users.noreply.github.com>
2019-08-05 23:38:47 -0700

    - One more try

--- bin/status-right.sh
+++ bin/status-right.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env zsh
 
-STATUS_LINE='#[fg=#4800b5,bg=#4800b5,nobold,nounderscore,noitalics]#[fg=colour121,bg=#4800b5] %a  %b %d  %r #[fg=#6e61ff,bg=#4800b5,nobold,nounderscore,noitalics]#[fg=#ffffff,bg=#6e61ff] #h'
+function ssh_user_online() {
+  who | grep '([0-9]\+\.[0-9]\+\.[0-9]\+\.[0-9]\+)' 2>&1 >/dev/null
+}
+
+STATUS_LINE='#[fg=#4800b5,bg=#4800b5,nobold,nounderscore,noitalics]#[fg=colour121,bg=#4800b5] %a  %b %d  %r #[fg=#6e61ff,bg=#4800b5,nobold,nounderscore,noitalics]#[fg=#ffffff,bg=#6e61ff] '
+
+if ssh_user_online; then
+  STATUS_LINE+='#H'
+else
+  STATUS_LINE+='#h'
+fi
 
 if [ -d ~/.task ]; then
   STATUS_LINE+='#[fg=#85ff9f,bg=#6e61ff,nobold,nounderscore,noitalics]#[fg=black,bg=#91ff93]#{tasks_status}'
