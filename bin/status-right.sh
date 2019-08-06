64d05d5d920c0f33a6eedf6a44c0d6268fd3be06
stevenwoah <stevenwoah@users.noreply.github.com>
2019-08-05 23:25:48 -0700

    Can't detect SSH in a tmux run-shell session, maybe

--- bin/status-right.sh
+++ bin/status-right.sh
@@ -1,12 +1,6 @@
 #!/usr/bin/env zsh
 
-STATUS_LINE='#[fg=#4800b5,bg=#4800b5,nobold,nounderscore,noitalics]#[fg=colour121,bg=#4800b5] %a  %b %d  %r #[fg=#6e61ff,bg=#4800b5,nobold,nounderscore,noitalics]#[fg=#ffffff,bg=#6e61ff] '
-
-if (( ${+SSH_CONNECTION} )); then
-  STATUS_LINE+="#H"
-else
-  STATUS_LINE+="#h"
-fi
+STATUS_LINE='#[fg=#4800b5,bg=#4800b5,nobold,nounderscore,noitalics]#[fg=colour121,bg=#4800b5] %a  %b %d  %r #[fg=#6e61ff,bg=#4800b5,nobold,nounderscore,noitalics]#[fg=#ffffff,bg=#6e61ff] #h'
 
 if [ -d ~/.task ]; then
   STATUS_LINE+='#[fg=#85ff9f,bg=#6e61ff,nobold,nounderscore,noitalics]#[fg=black,bg=#91ff93]#{tasks_status}'
